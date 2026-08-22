a0911fcf4d1de2c09423f3066815cc2593b923bb
APommer <luizarthurpommer@gmail.com>
2026-08-22 13:53:35 -0300

    O Value Object Money foi criado, mas ainda falta validações em seu construtor para garantir que ele(objeto Money) nunca fique em um estado inválido, na próxima mudança, ele será refatorado para a sintaxe moderna Java usando record pra value objects bem como suas validações necessárias serão adicionadas

--- src/com/financetracker/domain/Money.java
+++ src/com/financetracker/domain/Money.java
@@ -1,5 +1,22 @@
 package com.financetracker.domain;
 
-public class Money {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+public class Money {
+      private BigDecimal amount;
+      
+      public Money(BigDecimal value) {
+    	  this.amount = normalizeScale(value);
+      }
+      public Money add(BigDecimal value) {
+    	   return new Money(value.add(amount));
+    	  
+      }
+      public Money subtract(BigDecimal value) {
+    	   return new Money(value.subtract(amount));
+      }
+      private BigDecimal normalizeScale(BigDecimal value) {
+    	    return amount = value.setScale(2, RoundingMode.HALF_UP);
+      }
 }
