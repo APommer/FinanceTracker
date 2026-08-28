331c310b90b5f247f2959e9fc5e012d69d637017
APommer <luizarthurpommer@gmail.com>
2026-08-28 19:51:15 -0300

    feat: Category foi adicionada apesar de incompleta, ela é um Value Object com um Query Method isExpense(), após o protótipo inicial estiver completo, será feita uma refatoração completando classes que ainda não estão 100% feitas como o caso aqui e no commit anterior

--- src/com/financetracker/domain/Category.java
+++ src/com/financetracker/domain/Category.java
@@ -1,5 +1,18 @@
 package com.financetracker.domain;
 
 public class Category {
-
+	  private Type type;
+       private String name;
+       public enum Type{
+    	   EXPENSE, REVENUE
+       }
+       
+       public Category(String name, Type type) {
+    	    this.name = name;    
+    	    this.type = type;
+    	    }
+       
+       public boolean isExpense() {
+    	   return type == Type.EXPENSE;
+       }
 }
